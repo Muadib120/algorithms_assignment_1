903acf2908bd32161d1b54f49e83f79f7bf6d974
Benjamin <s3980858@student.rmit.edu.au>
2024-04-03 16:20:02 +1100

    Base implementation of AdjList class, not fully functioning, ie all cells have walls at the moment

--- maze/adjListGraph.py
+++ maze/adjListGraph.py
@@ -21,67 +21,120 @@ class AdjListGraph(Graph):
 
     def __init__(self):
         ### Implement me! ###
-        pass
+        self.graph = {}
 
 
         
     def addVertex(self, label:Coordinates):
         ### Implement me! ###
-        pass
-
+        if label not in self.graph:
+            self.graph[label] = list()
 
 
     def addVertices(self, vertLabels:List[Coordinates]):
         ### Implement me! ###
-        pass
+        for label in vertLabels:
+            if label not in self.graph:
+                self.graph[label] = list()
 
 
 
     def addEdge(self, vert1:Coordinates, vert2:Coordinates, addWall:bool = False)->bool:
         ### Implement me! ###
         # remember to return booleans
-        pass
+        edgeNotValid = False
+
+        for edge in self.graph[vert1]:
+            if vert2 in edge:
+                edgeNotValid = True
+
+        for edge in self.graph[vert2]:
+            if vert1 in edge:
+                edgeNotValid = True
+        
+        if not edgeNotValid and vert1.isAdjacent(vert2):
+            self.graph[vert1].append({vert2:addWall})
+            self.graph[vert2].append({vert1:addWall})
+        
+        return not edgeNotValid
         
 
 
     def updateWall(self, vert1:Coordinates, vert2:Coordinates, wallStatus:bool)->bool:
         ### Implement me! ###
         # remember to return booleans
-        pass
+        edgeFound = False
+        if vert1.isAdjacent(vert2):
+            for edge in self.graph[vert1]:
+                if vert2 in edge:
+                    edge[vert2] = wallStatus
+                    edgeFound = True
+
+            for edge in self.graph[vert2]:
+                if vert1 in edge:
+                    edge[vert1] = wallStatus
+                    edgeFound = True
+
+        return edgeFound
 
 
 
     def removeEdge(self, vert1:Coordinates, vert2:Coordinates)->bool:
         ### Implement me! ###
         # remember to return booleans
-        pass
-        
+        edgeRemoved = False
+        if vert1.isAdjacent(vert2):
+            for edge in self.graph[vert1]:
+                if vert2 in edge:
+                    del edge
+                    edgeRemoved = True
+            for edge in self.graph[vert2]:
+                if vert1 in edge:
+                    del edge
+                    edgeRemoved = True
+                    print("edge removed")
+        return edgeRemoved 
 
 
     def hasVertex(self, label:Coordinates)->bool:
         ### Implement me! ###
         # remember to return booleans
-        pass
+        return label in self.graph
 
 
 
     def hasEdge(self, vert1:Coordinates, vert2:Coordinates)->bool:
         ### Implement me! ###
         # remember to return booleans
-        pass
+        hasEdge = False
+
+        for edge in self.graph[vert1]:
+            if vert2 in edge:
+                hasEdge = True
+            
+        return hasEdge
 
 
 
     def getWallStatus(self, vert1:Coordinates, vert2:Coordinates)->bool:
         ### Implement me! ###
         # remember to return booleans
-        pass
-        
-    
+        wallstatusRetrieved = False
 
+        for edge in self.graph[vert1]:
+            if vert2 in edge:
+                wallstatusRetrieved = True
+        
+        return wallstatusRetrieved
+    
 
     def neighbours(self, label:Coordinates)->List[Coordinates]:
         ### Implement me! ###
         # remember to return list of coordinates
-        pass
+        coords = list()
+        for edge in self.graph[label]:
+            for coordinate in edge:
+                coords.append(coordinate)
+
+        return coords
         
